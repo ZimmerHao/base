89c25df41abc480c586c1f646377e06b7b888bb2
zimmer <zimmer.hao@gmail.com>
2016-03-03 19:45:37 +0800

    add fonts and images, test gulp-compass and gulp-jshint, global tasks are approved

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp   = require('gulp');
 var compass = require('gulp-compass');
-
+var cleanCSS = require('gulp-clean-css');
+var jshint = require('gulp-jshint');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var notify = require("gulp-notify");
 
 gulp.task('compass', function() {
   gulp.src('./assets/sass/*.scss')
@@ -11,4 +15,38 @@ gulp.task('compass', function() {
       style: 'compact'
     }))
     //.pipe(gulp.dest('./assets/build/css'));
+});
+
+gulp.task('global-compass', function() {
+  gulp.src('./assets/sass/global/*.scss')
+    .pipe(compass({
+      css: './assets/build/global/css',
+      sass: './assets/sass/global',
+      //sourcemap: true,
+      style: 'compact'
+    }))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./assets/build/global/css'));
+});
+
+gulp.task('js', function() {
+  return gulp.src('./assets/js/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./assets/build/js/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('./assets/build/js/'))
+    .pipe(notify({ message: 'js task complete' }));
+});
+
+gulp.task('global-js', function() {
+  return gulp.src('./assets/js/global/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('./assets/build/global/js/'))
+    .pipe(notify({ message: 'global js task complete' }));
 });
