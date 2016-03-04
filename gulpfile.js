74a6aa0dfc23475e729c119bca299d9d625635ea
zimmer <zimmer.hao@gmail.com>
2016-03-04 14:03:14 +0800

    change the structure of buid directory

--- gulpfile.js
+++ gulpfile.js
@@ -6,47 +6,27 @@ var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 var notify = require("gulp-notify");
 
-gulp.task('compass', function() {
-  gulp.src('./assets/sass/*.scss')
+
+gulp.task('css', function() {
+  gulp.src('./assets/sass/**/*.scss')
     .pipe(compass({
       css: './assets/build/css',
       sass: './assets/sass',
-      sourcemap: true, //compass 1.0 sourcemap
-      style: 'compact'
-    }))
-    //.pipe(gulp.dest('./assets/build/css'));
-});
-
-gulp.task('global-compass', function() {
-  gulp.src('./assets/sass/global/*.scss')
-    .pipe(compass({
-      css: './assets/build/global/css',
-      sass: './assets/sass/global',
       //sourcemap: true,
       style: 'compact'
     }))
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./assets/build/global/css'));
+    .pipe(gulp.dest('./assets/build/css'))
+    .pipe(notify({ message: 'css task complete' }));
 });
+
 
 gulp.task('js', function() {
   return gulp.src('./assets/js/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('./assets/build/js/'))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
-    .pipe(gulp.dest('./assets/build/js/'))
+    .pipe(gulp.dest('./assets/build/js'))
     .pipe(notify({ message: 'js task complete' }));
-});
-
-gulp.task('global-js', function() {
-  return gulp.src('./assets/js/global/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('./assets/build/global/js/'))
-    .pipe(notify({ message: 'global js task complete' }));
 });
